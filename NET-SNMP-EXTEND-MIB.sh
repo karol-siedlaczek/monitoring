d0ade046288f730d492804832613e90c63b4a2f8
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-05-20 18:05:56 +0200

    Update NET-SNMP-EXTEND-MIB.sh

--- NET-SNMP-EXTEND-MIB.sh
+++ NET-SNMP-EXTEND-MIB.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+SNMP_HOST=$1
+SNMP_PORT=$2
+SNMP_USER=$3
+SNMP_PASS=$4
+EXTEND_NAME=$5
+
+if [[ -z "$SNMP_HOST" || -z "$SNMP_PORT" || -z "$SNMP_USER" || -z "$SNMP_PASS" || -z "$EXTEND_NAME" ]]
+then
+   echo -e "Syntax error\nUsage: $0 <host> <port> <user> <pass> <extend>"
+   exit 0
+fi
+
 # For output
-/usr/bin/snmpget -t 10 -OQv -l authPriv -u <snmp_user> -A <snmp_pass> -X <snmp_pass> <snmp_host>:<snmp_port> NET-SNMP-EXTEND-MIB::nsExtendOutputFull.\"<extend_check_name>\"
+output=$(/usr/bin/snmpget -t 10 -OQv -l authPriv -u $SNMP_USER -A $SNMP_PASS -X $SNMP_PASS $SNMP_HOST:$SNMP_PORT NET-SNMP-EXTEND-MIB::nsExtendOutputFull.\"$EXTEND_NAME\")
 
 # For exit code
-/usr/bin/snmpget -t 10 -OQv -l authPriv -u <snmp_user> -A <snmp_pass> -X <snmp_pass> <snmp_host>:<snmp_port> NET-SNMP-EXTEND-MIB::nsExtendResult.\"<extend_check_name>\"
+result=$(/usr/bin/snmpget -t 10 -OQv -l authPriv -u $SNMP_USER -A $SNMP_PASS -X $SNMP_PASS $SNMP_HOST:$SNMP_PORT NET-SNMP-EXTEND-MIB::nsExtendResult.\"$EXTEND_NAME\")
+
+echo $output
+echo $result
