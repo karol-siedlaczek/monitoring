01f267a7def4b642467b19ec8ccc2eb4045c3651
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-05-20 11:29:57 +0200

    Update NET-SNMP-EXTEND-MIB.sh

--- NET-SNMP-EXTEND-MIB.sh
+++ NET-SNMP-EXTEND-MIB.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # For output
 /usr/bin/snmpget -t 10 -OQv -l authPriv -u <snmp_user> -A <snmp_pass> -X <snmp_pass> <snmp_host>:<snmp_port> NET-SNMP-EXTEND-MIB::nsExtendOutputFull.\"<extend_check_name>\"
 
