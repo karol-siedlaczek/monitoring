b2ac20fcf655edcd572aa1043a1f69e971f9f783
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-05-20 11:29:10 +0200

    Create NET-SNMP-EXTEND-MIB.sh

---
+++ NET-SNMP-EXTEND-MIB.sh
@@ -0,0 +1,5 @@
+# For output
+/usr/bin/snmpget -t 10 -OQv -l authPriv -u <snmp_user> -A <snmp_pass> -X <snmp_pass> <snmp_host>:<snmp_port> NET-SNMP-EXTEND-MIB::nsExtendOutputFull.\"<extend_check_name>\"
+
+# For exit code
+/usr/bin/snmpget -t 10 -OQv -l authPriv -u <snmp_user> -A <snmp_pass> -X <snmp_pass> <snmp_host>:<snmp_port> NET-SNMP-EXTEND-MIB::nsExtendResult.\"<extend_check_name>\"
