bb677b94e428dd6a902cdc6f767ecb21e8285ae3
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-11-11 23:31:00 +0100

    Create check_partition_mountpoint.sh

---
+++ check_partition_mountpoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# check if partition is mounted by uuid
+
+# nagios exit codes
+OK=0
+WARN=1
+CRITICAL=2
+UNKNOWN=3
+
+exit_code=$OK
+
+for uuid in "$@"
+do
+  mountpoint=$(lsblk -o MOUNTPOINT "/dev/disk/by-uuid/$uuid" | awk 'NR==2')
+  name=$(lsblk -o NAME "/dev/disk/by-uuid/$uuid" | awk 'NR==2')
+  if [ -z $name ]  # partition not found
+  then
+    output="${output}UNKNOWN: $uuid not a block device\n"
+    if [ $exit_code != $CRITICAL ]
+    then
+      exit_code=$UNKNOWN
+    fi
+  elif [[ -n $mountpoint ]]  # partition is mounted
+  then
+    output="${output}OK: /dev/$name mounted in $mountpoint\n"
+  else   # partition is not mounted
+    output="${output}CRITICAL: /dev/$name not mounted\n"
+    exit_code=$CRITICAL
+  fi
+done
+
+echo ${output%??}
+exit $exit_code
