41c406e2cbcdec5a5c691e4b4c1b87da02bd0948
Karol Siedlaczek <karol.siedlaczek@redge.com>
2026-07-18 01:45:21 +0200

    Edits ssl cert script

--- remote_checks/check_ssl_cert.sh
+++ remote_checks/check_ssl_cert.sh
@@ -14,64 +14,68 @@
 _HOST=""
 _WARNEXPIRYDAYS=""
 _CRITEXPIRYDAYS=""
+_PORT=""
 
-while getopts "h:w:c:" opt
+while getopts "h:w:c:p:" opt
     do
     case $opt in
         h ) _HOST=$OPTARG;;
         w ) _WARNEXPIRYDAYS=$OPTARG;;
         c ) _CRITEXPIRYDAYS=$OPTARG;;
+        p ) _PORT=$OPTARG;;
     esac
 done
 
+# Port is optional; default to 443 so existing (HTTPS) callers keep working.
+_PORT=${_PORT:-443}
+
 if [ ! "$_HOST" ]
     then
-    printf "ERROR - Either give Hostname in syntax as www.example.com or example.com with -h!\n"
+    printf "UNKNOWN: Either give Hostname in syntax as www.example.com or example.com with -h!\n"
     exit 3
 fi
 if [ ! "$_WARNEXPIRYDAYS" ]
     then
-    printf "ERROR - Add WARNING expiry in days with -w\n"
+    printf "UNKNOWN: Add WARNING expiry in days with -w\n"
     exit 3
 fi
 if [ ! "$_CRITEXPIRYDAYS" ]
     then
-    printf "ERROR - Add CRITICAL expiry in days with -c\n"
+    printf "UNKNOWN: Add CRITICAL expiry in days with -c\n"
     exit 3
 fi
 
-EXPIRYDATE=`echo "QUIT" | openssl s_client -connect $_HOST:443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null|sed 's/notAfter=//g'`
+EXPIRYDATE=`echo "QUIT" | timeout 10 openssl s_client -connect $_HOST:$_PORT -servername $_HOST 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null|sed 's/notAfter=//g'`
 #echo $EXPIRYDATE
 
-EXPIRYDATE_epoch=$(date --date "$EXPIRYDATE" +%s)
+if [ ! "$EXPIRYDATE" ]
+    then
+    echo "UNKNOWN: could not retrieve certificate from $_HOST:$_PORT"
+    exit 3
+fi
 
+EXPIRYDATE_epoch=$(date --date "$EXPIRYDATE" +%s)
 CURRENT_DATE_epoch=`date +%s`
 
-#echo $EXPIRYDATE_epoch
-#echo $CURRENT_DATE_epoch
-#echo $dayDiff
+### Expiry date as ISO (YYYY-MM-DD) for the message
+EXPIRY_ISO=`date --date "@$EXPIRYDATE_epoch" +%Y-%m-%d`
 
 epochDiff=`echo "$EXPIRYDATE_epoch" - "$CURRENT_DATE_epoch"|bc`
-#echo $epochDiff
 
 ### Get difference of days
 dayDiff=`echo "$epochDiff"/86400|bc`
-#echo $dayDiff
 
 if [ "$dayDiff" -le "$_CRITEXPIRYDAYS" ]
 then
-    echo "CRITICAL: $dayDiff days are left on $_HOST"
+    echo "CRITICAL: $_HOST:$_PORT certificate expires $EXPIRY_ISO (in $dayDiff days)"
     exit 2
 else
     if [  "$dayDiff" -le "$_WARNEXPIRYDAYS" ]
     then
-        echo  "WARNING: $dayDiff days are left on $_HOST"
+        echo "WARNING: $_HOST:$_PORT certificate expires $EXPIRY_ISO (in $dayDiff days)"
         exit 1
     else
-        if [ "$dayDiff" -gt "$_WARNEXPIRYDAYS" ]
-        then
-            echo "OK: $dayDiff days are left on $_HOST"
-            exit 0
-        fi
+        echo "OK: $_HOST:$_PORT certificate expires $EXPIRY_ISO (in $dayDiff days)"
+        exit 0
     fi
 fi
